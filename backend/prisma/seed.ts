0c60ba03347e71068de03afa32dda6c6b91cec1b
Krishna Gupta <kgupta@ibm.com>
2026-08-08 23:42:59 +0530

    added seeds

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,68 @@
+import { PrismaClient, Role, SeatStatus, SeatZone } from "@prisma/client";
+import bcrypt from "bcrypt";
+const prisma = new PrismaClient();
+
+async function main() {
+    const password = await bcrypt.hash("password123", 10);
+    //create admin and user
+    await prisma.user.createMany({
+    data: [
+        {
+        employeeId: "EMP002",
+        name: "Krishna Gupta",
+        email: "krishna@company.com",
+        password,
+        role: Role.EMPLOYEE
+        },
+        {
+        employeeId: "EMP003",
+        name: "John Doe",
+        email: "john@company.com",
+        password,
+        role: Role.EMPLOYEE
+        },
+        {
+            name: "System Admin",
+            email: "admin@blureserve.com",
+            password,
+            role: Role.ADMIN
+
+        }
+    ]
+    });
+
+    const cafeteria = await prisma.cafeteria.create({
+    data: {
+        name: "Main Cafeteria",
+        building: "Building A",
+        floor: 1,
+        capacity: 50
+    }
+    });
+
+    const seats = [];
+    for(let i=1;i<=50;i++){
+        seats.push({
+        seatNumber: `A${i}`,
+        cafeteriaId: cafeteria.id,
+        zone: SeatZone.CENTER,
+        status: SeatStatus.AVAILABLE,
+        isWindowSeat: i <= 5
+        });
+    }
+    await prisma.seat.createMany({
+    data:seats
+});
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+
+}
